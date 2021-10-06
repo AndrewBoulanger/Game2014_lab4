9da00369432594885273bafe19d756140ca753b0
Andrew Boulanger <andrew_boulanger14@hotmail.com>
2021-10-06 02:03:11 -0400

    added player movement and bounds

---
+++ Assets/[Scripts]/PlayerBehaviour.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBehaviour : MonoBehaviour
+{
+    [Header("Player Movement")]
+    [Range(0.0f, 100f)]
+    public float horizontalForce;
+    private Rigidbody2D rigidbody;
+    [Range (0f, 1f)]
+    public float decay;
+    public Bounds bounds;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+
+
+
+    private void FixedUpdate()
+    {
+        Move();
+        CheckBounds();
+    }
+
+    private void Move()
+    {
+        var x = Input.GetAxisRaw("Horizontal");
+
+        rigidbody.AddForce(new Vector2(x * horizontalForce, 0.0f));
+
+        rigidbody. velocity *= (1f - decay);
+;
+    }
+
+    private void CheckBounds()
+    {
+        if(transform.position.x < bounds.min)
+        {
+            transform.position = new Vector2(bounds.min, transform.position.y);
+        }
+        else if (transform.position.x > bounds.max)
+        {
+            transform.position = new Vector2(bounds.max, transform.position.y);
+        }
+    }
+}
